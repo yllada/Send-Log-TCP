a4c1862245d5d759ba19e1015c41e2304954d83e
Yadian Llada Lopez <yadian.llada@gmail.com>
2024-10-06 10:54:07 -0400

    refactor processor

---
+++ processor/buildSyslogMessage.go
@@ -0,0 +1,16 @@
+package processor
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/yllada/Send-Log-TCP/schema"
+)
+
+func BuildSyslogMessage(config schema.SyslogConfig, message string) string {
+	timestamp := time.Now().Format("2006-01-02T15:04:05.000Z")
+	pri := (config.Facility * 8) + config.Severity
+
+	// Añadir CRLF al final del mensaje
+	return fmt.Sprintf("%d <%d>1 %s %s %s\r\n", 679, pri, timestamp, config.Hostname, message)
+}
--- processor/process.go
+++ processor/process.go
@@ -4,53 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"os"
 	"time"
 
 	"github.com/yllada/Send-Log-TCP/schema"
 )
 
-func BuildSyslogMessage(config schema.SyslogConfig, message string) string {
-	timestamp := time.Now().Format("2006-01-02T15:04:05.000Z")
-	pri := (config.Facility * 8) + config.Severity
-
-	// Añadir CRLF al final del mensaje
-	return fmt.Sprintf("%d <%d>1 %s %s %s\r\n", 679, pri, timestamp, config.Hostname, message)
-}
-
-func setDefaultSyslogConfig(config *schema.SyslogConfig) {
-	// Protocolo, Address, Port y Messages son requeridos, no necesitan valores por defecto
-
-	// Si Hostname está vacío, intentar obtener el nombre del host desde el sistema operativo
-	if config.Hostname == "" {
-		hostname, err := os.Hostname()
-		if err != nil {
-			log.Printf("Error retrieving hostname: %v", err)
-			config.Hostname = "unknown-host" // Valor por defecto si no se puede obtener
-		} else {
-			config.Hostname = hostname
-		}
-	}
-
-	// Si Facility es 0 (no se proporcionó), asignar el valor por defecto 1
-	if config.Facility == 0 {
-		config.Facility = 1
-	}
-
-	// Si Severity es 0 (no se proporcionó), asignar el valor por defecto 5 (informativo)
-	if config.Severity == 0 {
-		config.Severity = 5
-	}
-
-	// Si Interval es 0, asignar un valor por defecto de 1 segundo
-	if config.Interval == 0 {
-		config.Interval = 1 * time.Second
-	}
-}
-
 func SendSyslogMessages(config schema.SyslogConfig) {
 	// Establecer los valores por defecto si no se proporcionaron
-	setDefaultSyslogConfig(&config)
+	SetDefaultSyslogConfig(&config)
 	// Concatenar la dirección con el puerto
 	fullAddress := fmt.Sprintf("%s:%s", config.Address, config.Port)
 
