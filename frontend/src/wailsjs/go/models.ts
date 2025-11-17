e4937bec361a2d39881c2e38b408906cb6813be3
JocLRojas <joc.l.rojas02@gmail.com>
2025-11-17 18:03:53 -0500

    chore(frontend): update Wails generated types
    - Update SyslogConfig with new fields
    - Fix WindowSetSize return type

--- frontend/src/wailsjs/go/models.ts
+++ frontend/src/wailsjs/go/models.ts
@@ -5,6 +5,12 @@ export namespace main {
 	    Port: string;
 	    Protocol: string;
 	    Messages: string[];
+	    FramingMethod: string;
+	    Facility: number;
+	    Severity: number;
+	    Hostname: string;
+	    Appname: string;
+	    UseRFC5424: boolean;
 	
 	    static createFrom(source: any = {}) {
 	        return new SyslogConfig(source);
@@ -16,6 +22,12 @@ export namespace main {
 	        this.Port = source["Port"];
 	        this.Protocol = source["Protocol"];
 	        this.Messages = source["Messages"];
+	        this.FramingMethod = source["FramingMethod"];
+	        this.Facility = source["Facility"];
+	        this.Severity = source["Severity"];
+	        this.Hostname = source["Hostname"];
+	        this.Appname = source["Appname"];
+	        this.UseRFC5424 = source["UseRFC5424"];
 	    }
 	}
 	export class SyslogResponse {
